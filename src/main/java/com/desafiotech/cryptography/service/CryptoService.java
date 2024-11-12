771cd0f4cfdde6193a3cc607d87d17ab904a0df7
Pedro Henrique Basilio <phbasilio6903@gmail.com>
2024-11-12 16:28:24 -0300

    implementando cryptoService para teste de encriptação e descriptação

---
+++ src/main/java/com/desafiotech/cryptography/service/CryptoService.java
@@ -0,0 +1,24 @@
+package com.desafiotech.cryptography.service;
+
+import org.jasypt.util.text.StrongTextEncryptor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CryptoService {
+
+    private static final StrongTextEncryptor encryptor;
+
+    static {
+        encryptor = new StrongTextEncryptor();
+        encryptor.setPassword(System.getenv("APP_KEY"));
+    }
+
+    public static String encrypt (String rawText){
+        return encryptor.encrypt(rawText);
+    }
+
+
+    public static String decrypt( String encryptedText){
+        return encryptor.decrypt(encryptedText);
+    }
+}
